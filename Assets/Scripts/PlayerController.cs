508cf8640cccb564c5dc95048d65ab3a5516e234
Euliya <148483694+Euliya@users.noreply.github.com>
2024-11-23 23:34:08 -0500

    Journal 6 done

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -1,3 +1,5 @@
+using System.Collections;
+using System.Collections.Generic;
 using UnityEngine;
 
 public class PlayerController : MonoBehaviour
@@ -6,11 +8,26 @@ public class PlayerController : MonoBehaviour
     {
         left, right
     }
+    FacingDirection direction;
+
+    public float accTime;
+    public float maxSpeed;
+    public float decTime;
+
+    public float ray;
+
+    private float acc;
+    private float dec;
+    private Rigidbody2D rb;
+
+
 
     // Start is called before the first frame update
     void Start()
     {
-        
+        acc = maxSpeed / accTime;
+        dec = maxSpeed / accTime;
+        rb = GetComponent<Rigidbody2D>();
     }
 
     // Update is called once per frame
@@ -19,26 +36,42 @@ public class PlayerController : MonoBehaviour
         // The input from the player needs to be determined and
         // then passed in the to the MovementUpdate which should
         // manage the actual movement of the character.
-        Vector2 playerInput = new Vector2();
+        Vector2 playerInput = new Vector2(Input.GetAxis("Horizontal"), 0);
         MovementUpdate(playerInput);
     }
 
     private void MovementUpdate(Vector2 playerInput)
     {
+        Vector2 currentV = rb.velocity;
 
+        if (playerInput != Vector2.zero)
+        {
+            direction = playerInput.x < 0 ? FacingDirection.left : FacingDirection.right;
+            currentV += acc * playerInput * Time.deltaTime;
+            currentV.x = Mathf.Clamp(currentV.x, -maxSpeed, maxSpeed);
+        }
+
+        else if (currentV.magnitude > 0)
+        {
+            currentV = currentV.normalized * Mathf.Clamp(Mathf.Abs(currentV.x) - dec * Time.deltaTime, 0, maxSpeed);
+        }
+
+        rb.velocity = currentV;
     }
 
     public bool IsWalking()
     {
-        return false;
+        return rb.velocity.x != 0;
     }
     public bool IsGrounded()
     {
-        return false;
+        Debug.DrawRay(transform.position, Vector2.down * ray, Color.red);
+        RaycastHit2D hit = Physics2D.Raycast(transform.position, Vector2.down, ray);
+        return hit.collider != null;
     }
 
     public FacingDirection GetFacingDirection()
     {
-        return FacingDirection.left;
+        return direction;
     }
 }
