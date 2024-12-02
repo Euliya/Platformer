cf4e3f4d0bddec7233432cba95b2fbac9033d9ef
ChenWangzichen <148483694+Euliya@users.noreply.github.com>
2024-12-01 19:24:59 -0500

    Journal 7 Done

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -1,5 +1,6 @@
 using System.Collections;
 using System.Collections.Generic;
+using JetBrains.Annotations;
 using UnityEngine;
 
 public class PlayerController : MonoBehaviour
@@ -20,7 +21,17 @@ public class PlayerController : MonoBehaviour
     private float dec;
     private Rigidbody2D rb;
 
+    public float apexH;
+    public float apexT;
+    public float termSpeed;
+    public float coyoteT;
 
+    private float jumpGrv;
+    private float grv;
+    private float iniJumpV;
+    private bool IsJump = false;
+    [SerializeField]private bool IsCoyote = false;
+    private bool onGround;
 
     // Start is called before the first frame update
     void Start()
@@ -28,6 +39,13 @@ public class PlayerController : MonoBehaviour
         acc = maxSpeed / accTime;
         dec = maxSpeed / accTime;
         rb = GetComponent<Rigidbody2D>();
+
+        grv = rb.gravityScale;
+        jumpGrv = -2 * apexH / Mathf.Pow(apexT, 2);
+        iniJumpV = 2 * apexH / apexT;
+
+        onGround = IsGrounded();
+
     }
 
     // Update is called once per frame
@@ -40,6 +58,7 @@ public class PlayerController : MonoBehaviour
         MovementUpdate(playerInput);
     }
 
+    
     private void MovementUpdate(Vector2 playerInput)
     {
         Vector2 currentV = rb.velocity;
@@ -51,12 +70,45 @@ public class PlayerController : MonoBehaviour
             currentV.x = Mathf.Clamp(currentV.x, -maxSpeed, maxSpeed);
         }
 
-        else if (currentV.magnitude > 0)
+        else if (Mathf.Abs(currentV.x )> 0)
         {
-            currentV = currentV.normalized * Mathf.Clamp(Mathf.Abs(currentV.x) - dec * Time.deltaTime, 0, maxSpeed);
+            currentV.x = currentV.normalized.x * Mathf.Clamp(Mathf.Abs(currentV.x) - dec * Time.deltaTime, 0, maxSpeed);
+        }
+        
+        if (!IsGrounded() && onGround)
+        {
+            StartCoroutine(coyoteTime());
+        }
+        onGround = IsGrounded();
+
+        if (Input.GetButton("Jump") && (IsGrounded()||IsCoyote))
+        {
+            IsJump = true;
+            IsCoyote = false;
+            rb.gravityScale = 0;
+            currentV.y = iniJumpV;
+            StartCoroutine(Falling());            
+        }
+        if (IsJump)
+        {
+            currentV.y += jumpGrv * Time.deltaTime;
         }
 
+        currentV.y = Mathf.Clamp(currentV.y, -termSpeed, iniJumpV);
+
         rb.velocity = currentV;
+    }
+    IEnumerator Falling()
+    {
+        yield return new WaitForSeconds(apexT);
+        rb.gravityScale = grv;
+        IsJump = false;
+    }
+    IEnumerator coyoteTime()
+    {
+        IsCoyote = true;
+        yield return new WaitForSeconds(coyoteT);
+        IsCoyote = false;
     }
 
     public bool IsWalking()
